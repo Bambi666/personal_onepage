8620f4cf42f7743b4e88b31fafae3b7465bb2809
bambi <sarnina@me.com>
2020-06-29 02:14:54 +0200

    Top bar animation on scroll.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let navigation = document.getElementById('navigation');
 window.addEventListener("scroll", function () {
-    if (window.pageYOffset > 0) {
+    if (window.pageYOffset > 50) {
         navigation.classList.add('active');
     } else {
         navigation.classList.remove('active');
