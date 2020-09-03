9d37f210bcbb25fd4567887a555c50ec8f848226
bambi <sarnina@me.com>
2020-09-03 11:45:20 +0200

    About me: tabs in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,62 +8,21 @@ window.addEventListener("scroll", function () {
 
 })
 
-
-
-// let specialHeading = document.querySelectorAll(".special-heading > ul > li");
-
-
-// window.addEventListener("load", function () {
-
-//     var specialHeadingIndex = 0;
-
-//     setInterval(function () {
-//         specialHeadingIndex++
-
-//         if (specialHeading.length <= specialHeadingIndex) {
-//             specialHeadingIndex = 0;
-//         }
-
-//         [...specialHeading].map((val, index) => {
-//             val.classList.remove('in');
-//             val.classList.add('out');
-
-//             specialHeading[specialHeadingIndex].classList.add('in');
-//             specialHeading[specialHeadingIndex].classList.remove('out');
-
-
-//         })
-
-
-//     }, 1000);
-
-// })
-
+// SPECIAL HEADINGS BIT ###############################
 
 let specialHeadings = ['UX Designer', 'Content Writer', 'JS Developer'];
-
 let specialHeadingsIndex = 0;
 
-
 setInterval(function () {
-
     if (specialHeadingsIndex >= specialHeadings.length) {
         specialHeadingsIndex = 0;
     }
-
     document.getElementById('special-heading').innerHTML = specialHeadings[specialHeadingsIndex];
-
-
-    console.log(specialHeadings[specialHeadingsIndex]);
     specialHeadingsIndex += 1;
-
-
 }, 3000);
 
-let _counter = 0;
 
 setInterval(function () {
-
     if (document.getElementById('special-heading').classList.contains('reset')) {
         document.getElementById('special-heading').classList.remove('reset');
         document.getElementById('special-heading').classList.add('in');
@@ -76,8 +35,34 @@ setInterval(function () {
         document.getElementById('special-heading').classList.remove('out');
         document.getElementById('special-heading').classList.add('reset');
     };
-
 }, 1000);
 
+
+// ABOUT ME : TABS #####################################
+
+
+let tabsContentText = ["SKILLS", "AWARDS", "EXPERIENCE", "EDUCATION"];
+
+
+let skills = document.getElementById('skills');
+
+document.getElementById('tabs-content').innerHTML = tabsContentText[0];
+
+skills.addEventListener("click", function (e) {
+    skills.classList.add('tab-active');
+    awards.classList.remove('tab-active');
+    document.getElementById('tabs-content').innerHTML = tabsContentText[0];
+})
+
+let awards = document.getElementById('awards');
+awards.addEventListener("click", function () {
+    awards.classList.add('tab-active');
+    skills.classList.remove('tab-active');
+    document.getElementById('tabs-content').innerHTML = tabsContentText[1];
+})
+
+// let tabsContent = document.getElementsByClassName('tabs-content');
+
+// document.getElementById('tabs-content').innerHTML = tabsContentText[0];
 
 // AOS.init();
