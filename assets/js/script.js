dcdad851e04ef7e7cc1171ed9caac0b70d8238df
bambi <sarnina@me.com>
2020-09-03 16:41:04 +0200

    About me: working tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,28 +41,49 @@ setInterval(function () {
 // ABOUT ME : TABS #####################################
 
 
-let tabsContentText = ["SKILLS", "AWARDS", "EXPERIENCE", "EDUCATION"];
+// let tabsContentText = ["SKILLS", "AWARDS", "EXPERIENCE", "EDUCATION"];
 
 
-let skills = document.getElementById('skills');
+// let skills = document.getElementById('skills');
 
-document.getElementById('tabs-content').innerHTML = tabsContentText[0];
+// document.getElementById('tabs-content').innerHTML = tabsContentText[0];
 
-skills.addEventListener("click", function (e) {
-    skills.classList.add('tab-active');
-    awards.classList.remove('tab-active');
-    document.getElementById('tabs-content').innerHTML = tabsContentText[0];
-})
+// skills.addEventListener("click", function (e) {
+//     skills.classList.add('tab-active');
+//     awards.classList.remove('tab-active');
+//     document.getElementById('tabs-content').innerHTML = tabsContentText[0];
+// })
 
-let awards = document.getElementById('awards');
-awards.addEventListener("click", function () {
-    awards.classList.add('tab-active');
-    skills.classList.remove('tab-active');
-    document.getElementById('tabs-content').innerHTML = tabsContentText[1];
-})
+// let awards = document.getElementById('awards');
+// awards.addEventListener("click", function () {
+//     awards.classList.add('tab-active');
+//     skills.classList.remove('tab-active');
+//     document.getElementById('tabs-content').innerHTML = tabsContentText[1];
+// })
 
 // let tabsContent = document.getElementsByClassName('tabs-content');
 
 // document.getElementById('tabs-content').innerHTML = tabsContentText[0];
+
+// BETTER TABS ##################################
+
+let tabs = document.getElementsByClassName('tabs')[0];
+
+tabs.addEventListener("click", function (event) {
+
+    if (event.target.className == 'tab') {
+
+        tabs.querySelectorAll('button.tab[data-id], div.tabs-content[data-id]').forEach(element => {
+            element.classList.remove('active');
+
+        });
+        let dataId = event.target.getAttribute('data-id');
+
+        tabs.querySelector('div.tabs-content[data-id=' + dataId + ']').classList.add('active');
+        tabs.querySelector('button.tab[data-id=' + dataId + ']').classList.add('active');
+
+    };
+});
+
 
 // AOS.init();
